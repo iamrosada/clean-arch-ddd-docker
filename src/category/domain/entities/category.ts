711d4a32d827eeec0d508c420e6d8461688b81e2
iamrosada <luisrosada2018@gmail.com>
2023-05-28 14:35:01 +0300

    feat: created logic to receive optional properties

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -6,5 +6,17 @@ export type CategoryProperties = {
 };
 
 export default class Category {
-  constructor(public readonly props: CategoryProperties) {}
+  constructor(public readonly props: CategoryProperties) {
+    this.props.description = this.props.description;
+    this.props.is_active = this.props.is_active ?? true;
+    this.props.created_at = this.props.created_at ?? new Date();
+  }
+
+  private set description(value: string) {
+    this.props.description = this.props.description ?? null;
+  }
+
+  private set is_active(value: boolean) {
+    this.props.is_active = this.props.is_active ?? true;
+  }
 }
