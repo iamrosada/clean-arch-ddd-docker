b1afcda9a0b80fd69bb8a9f43013218ce6ab6c34
iamrosada <luisrosada2018@gmail.com>
2023-05-28 14:17:57 +0300

    test: updated the method

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,9 +1,39 @@
 import Category from "./category";
 
-describe("Category Tests", () => {
+describe("Category Unit Tests", () => {
   test("constructor of category", () => {
-    const category = new Category("Movie");
+    const created_at = new Date();
+    //Arrange
+    const props = {
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at,
+    };
 
-    expect(category.name).toBe("Movie");
+    //Act
+    const category = new Category(props);
+
+    //Assert
+    // expect(category.props.name).toBe("Movie");
+    // expect(category.props.description).toBe("some description");
+    // expect(category.props.is_active).toBeTruthy();
+    // expect(category.props.created_at).toBe(props.created_at);
+
+    //outra forma tb de fazer o teste seria assim
+
+    // expect(category.props).toMatchObject({
+    //   name: "Movie",
+    //   description: "some description",
+    //   is_active: true,
+    //   created_at,
+    // });
+    // if was added a new variable into project this will check
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at,
+    });
   });
 });
