425a0af8fa88f687c4608c51c06336b05e88d34d
iamrosada <luisrosada2018@gmail.com>
2023-05-28 17:34:37 +0300

    feat: added getter and setter

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -7,16 +7,31 @@ export type CategoryProperties = {
 
 export default class Category {
   constructor(public readonly props: CategoryProperties) {
-    this.props.description = this.props.description;
+    this.props.description = this.props.description ?? null;
     this.props.is_active = this.props.is_active ?? true;
     this.props.created_at = this.props.created_at ?? new Date();
+  }
+
+  get name() {
+    return this.props.name;
+  }
+  get description() {
+    return this.props.description;
   }
 
   private set description(value: string) {
     this.props.description = this.props.description ?? null;
   }
 
+  get is_active() {
+    return this.props.is_active;
+  }
+
   private set is_active(value: boolean) {
     this.props.is_active = this.props.is_active ?? true;
+  }
+
+  get created_at() {
+    return this.props.created_at;
   }
 }
